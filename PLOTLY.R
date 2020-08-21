e5907737c0d4c94642f7bdd8d9f9c4e40a17ce60
Chevy9908 <70026680+Chevy9908@users.noreply.github.com>
2020-08-21 11:32:37 -0500

    Add files via upload

---
+++ PLOTLY.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+install.packages("ggplot2")
+install.packages("plotly")
+  library(plotly)
+install.packages("dplyr")
+library(gapminder)
+library("dplyr")
+data<-gapminder
+p<-ggplot(data=data, mapping = aes(x=gdpPercap, y=lifeExp))+geom_point()
+ggplotly(p)
+
+plot_ly(x=c("leones","orangutanes","monos"), y=c(5, 10, 7), type="bar")
+
+plot_ly(labels=c("leones","orangutanes","monos"), values=c(5, 10, 7), type="pie")
+
+data<-ggplot2:: diamonds
+View(diamonds)
+data<- data%>%count(cut,clarity)
+fig<-data%>% plot_ly(x=~cut, y=~n, color= ~clarity)
+fig
